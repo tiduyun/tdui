9e50bf4d002a1d5c6a9fa6d9ee6b4a347130d06a
Allex Wang <allex.wxn@gmail.com>
2021-08-03 13:41:49 +0800

    feat: add checkbox-group

---
+++ src/packages/Checkbox/CheckboxGroup.tsx
@@ -0,0 +1,17 @@
+import { functionalComponent } from '@/utils/vue'
+import { CreateElement } from 'vue'
+
+import { IOption } from '../../types/common'
+
+/**
+ * ```jsx
+ *  <ComponentBuilder
+ *    component="CheckboxGroup"
+ *    v-model="checkbox"
+ *    :options="[{ label: 't1', value: 'v1' }, { label: 't2', value: 'v2' }]"
+ *  />
+ * ```
+ */
+export const CheckboxGroup = functionalComponent((h: CreateElement, { options, ...props }) =>
+  (<el-checkbox-group {...props}>{options.map((o: IOption) => <el-checkbox key={o.value} label={o.value}>{o.label}</el-checkbox>)}</el-checkbox-group >)
+)
