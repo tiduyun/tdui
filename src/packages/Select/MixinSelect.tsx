06b2e8f6f584d8266d97ec9c403bc6132f5373c2
Allex Wang <allex.wxn@gmail.com>
2021-12-14 17:41:54 +0800

    chore: refactor mixin-select struct

--- src/packages/Select/MixinSelect.tsx
+++ src/packages/Select/MixinSelect.tsx
@@ -1,156 +1,76 @@
-import { constant, isArray, isEmpty, isFunction, isObject, isPromise, valueEquals } from '@tdio/utils'
-import { Component, Mixins, Prop, Vue, Watch } from 'vue-property-decorator'
+
+import { constant, isArray, isEmpty, isFunction, isObject, isPromise, keys, valueEquals } from '@tdio/utils'
+import { Component, Mixins, Prop, Ref, Vue } from 'vue-property-decorator'
+
+import { parseProps } from '../../utils/vue/parseProps'
 
 import { IOptionEntity, OptionsProvider } from './AbsSelectView'
-import Select from './Select'
+import { AsyncSelect } from './AsyncSelect'
 
 const values = (o: any) => (isObject(o) ? Object.values(o) : o)
 const isValueEquals = (o: any, p: any) => valueEquals(values(o), values(p))
 
-type IOptionsLoader = (...args: any[]) => Promise<IOptionEntity[]> | IOptionEntity[]
+type CacheKey = string | number | object
 
 @Component
 class MixinSelect extends Vue {
-  /** @abstract */
-  keys: string = ''
+  /**
+   * Fields for sub-component overrides
+   * @abstract
+   */
+  keys: CacheKey = ''
   props: Kv = {}
+  propLabel = 'label'
+  propValue = 'value'
 
-  propLabel: string = 'label'
-  propValue: string = 'value'
+  /* abstract options: OptionsProvider; */
 
-  @Prop()
-  value!: any
-
-  @Prop()
-  entity!: IOptionEntity
+  @Ref('async-select')
+  $sel!: AsyncSelect
 
   @Prop({ type: Function, default: constant(true) })
   optionsFilter!: (o: IOptionEntity) => boolean
 
-  @Prop(Boolean)
-  loading!: boolean
-
-  lock: boolean = this.loading
-  items: IOptionEntity[] = []
-
-  private _inited?: boolean // duplicate initialize fetching locker
-  private _pending?: IOptionsLoader
-
-  @Watch('lock')
-  emitLoading (v: boolean): void {
-    this.$emit('update:loading', v)
-  }
-
-  initOptions <T> (provider: OptionsProvider, force?: boolean) {
-    const { keys } = this
-    const v = provider
-
-    if (isFunction(v)) {
-      const f: IOptionsLoader = (v as IOptionsLoader).bind(this)
-      if (this.lock && !force) {
-        this._pending = f
-        return
-      }
-
-      this.lock = true
-
-      const handleCb = (err: Error | null, r?: IOptionEntity[]) => {
-        if (!err) {
-          if (!isEmpty(keys)) {
-            this._inited = true
-          }
-          this.setOptions(r || [])
-        }
-
-        this.lock = false
-
-        const pending = this._pending
-        if (pending) {
-          this._pending = undefined
-          if (!isValueEquals(keys, this.keys)) {
-            this.initOptions(pending)
-          }
-        }
-      }
-
-      const r = f(keys)
-      if (isPromise(r)) {
-        (r as Promise<IOptionEntity[]>).then(
-          arr => handleCb(null, arr),
-          err => handleCb(err)
-        )
-      } else {
-        handleCb(null, r as IOptionEntity[])
-      }
-
-    } else if (v !== this.items) {
-      this.setOptions(v as IOptionEntity[])
-    }
-  }
-
   /**
    * API for retrieve select options.
    */
-  load (force?: boolean) {
-    this.initOptions(this.options, force)
+  load () {
+    return this.$sel.load()
   }
 
   mounted () {
-    this.load(true)
     // Add a watcher to assert auto reload options
-    this.$watch('keys', (v: string | Kv, p: string | Kv) => {
-      if (!isEmpty(p) && this._inited && isValueEquals(v, p)) return
-      this.load()
-    })
-    this.$watch('options', () => {
+    this.$watch('keys', (v: CacheKey, p: CacheKey) => {
+      if (!isEmpty(p) && isValueEquals(v, p)) return
       this.load()
     })
   }
 
   render () {
-    const {
-      // attrs for internal select properties (forward)
-      lock: inputLoading,
+    const { options, propLabel, propValue, $slots, $scopedSlots } = this
+    if (!(isFunction(options) || isArray(options))) {
+      throw new Error('The abstract options not implement yet.')
+    }
 
-      // props
-      value,
-      entity,
+    const parsedProps = parseProps({
+      options,
+      optionsFilterFunc: this.optionsFilter,
       propLabel,
       propValue,
-      items: options,
-
-      // slots
-      $slots,
-      $scopedSlots,
-    } = this
-
-    const children = Object.keys($slots).map(slot => <template slot={slot}>{ $slots[slot] }</template>)
+      ...this.props,
+      ...this.$attrs
+    }, AsyncSelect)
 
     return (
-      <Select
-        attrs={{
-          ...this.props,
-          ...this.$attrs,
-          inputLoading
-        }}
-        props={{
-          value,
-          options,
-          propLabel,
-          propValue,
-          entity,
-        }}
+      <AsyncSelect
+        ref="async-select"
+        {...parsedProps}
         on={this.$listeners}
         scopedSlots={$scopedSlots}
       >
-        { children }
-      </Select>
+        { Object.keys($slots).map(slot => <template slot={slot}>{ $slots[slot] }</template>) }
+      </AsyncSelect>
     )
-  }
-
-  private setOptions (items: IOptionEntity[]): void {
-    items = isArray(items) ? items : []
-    this.items = items.filter(this.optionsFilter)
   }
 }
 
